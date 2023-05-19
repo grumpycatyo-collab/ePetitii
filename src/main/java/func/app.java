0b2d89436f170c879356e5d14bd0c0022c8b4c23
Nikolai (from gibraltar) <niksaysit@gmail.com>
2023-05-20 01:49:30 +0300

    for docker

--- src/main/java/func/app.java
+++ src/main/java/func/app.java
@@ -3,9 +3,9 @@ package func;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
-
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import java.util.List;
-@SpringBootApplication
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class})
 public class app {
     private final PetitionService petitionService;
 

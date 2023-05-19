9ac3645d1811ebabe937fcf271ecd35d6439b3b4
grumpycatyo-collab <77546227+grumpycatyo-collab@users.noreply.github.com>
2023-05-19 18:18:55 +0300

    Pulled users from MariaDB

--- src/main/java/func/UserService.java
+++ src/main/java/func/UserService.java
@@ -8,6 +8,7 @@ import java.util.List;
 public class UserService {
     private final UserRepository userRepository;
 
+
     public UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
